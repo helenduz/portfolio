cf855be5187a0d31f898f73cf3a255bc2a0b3a41
helenduz <duh2@carleton.edu>
2022-12-15 17:43:47 -0500

    Add redesign project page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Iterative from './components/ProjectPages/Iterative';
+import Redesign from './components/ProjectPages/Redesign';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/iterative" element={<Iterative/>}/>
+          <Route path="/redesign" element={<Redesign/>}/>
           {/* <Route path="/proj-2" element={<ProjectPageComponent2/>}/>
           <Route path="/proj-3" element={<ProjectPageComponent3/>}/>
           <Route path="/proj-4" element={<ProjectPageComponent4/>}/> */}
