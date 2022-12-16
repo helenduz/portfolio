2d0cd480b8cfaa64142688ea58b6eef7550e4ef2
helenduz <duh2@carleton.edu>
2022-12-16 14:06:24 -0500

    Fix router basename

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename='process.env.PUBLIC_URL'>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="app">
         <Header />
         <Routes>
